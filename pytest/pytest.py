de7a86e7b1fd540e0f1dc183452eb1a5f8ed685f
Kashishvarmaa <136305941+Kashishvarmaa@users.noreply.github.com>
2023-10-06 16:43:36 +0530

    Add files via upload

---
+++ pytest/pytest.py
@@ -0,0 +1,14 @@
+"""
+This is a simple script that adds two numbers and prints the result.
+"""
+
+# Constants should be in uppercase according to naming conventions.
+A = 1
+B = 2
+
+# Calculate the result of the addition.
+result = A + B
+
+# Print the result.
+print(result)
+
